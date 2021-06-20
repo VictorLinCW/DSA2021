dd9f393e0fe3c60a97ef5f3407b6962c102e8626
VictorLinCW <71741739+VictorLinCW@users.noreply.github.com>
2021-06-20 17:06:08 +0800

    Update HW3_6.c
    AC

--- HW3/HW3_6.c
+++ HW3/HW3_6.c
@@ -1,21 +1,21 @@
+// reference:https://www.geeksforgeeks.org/queue-linked-list-implementation/
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-
-typedef struct stack {
+typedef struct vertex {
     int top;
     unsigned capacity;
     int* array;
-}stack;
+}vertex;
 
-bool isStackFull(stack stack)
+bool isVertexFull(vertex vertex)
 {
-    return stack.top > stack.capacity - 1;
+    return vertex.top > vertex.capacity - 1;
 }
 
 typedef struct QNode {
-    int key;  //stack的index
+    int key;  //vertex的index
     struct QNode* next;
     struct QNode* prev;
 }QNode;
@@ -44,28 +44,38 @@ Queue* createQueue()
     return q;
 }
 
-
 Queue* enqueue(Queue* q, int k)
 {
     struct QNode* temp = newNode(k);
   
-    // If queue is empty
     if (q->rear == NULL)
     {
         q->front = q->rear = temp;
         q->size += 1;
         return q;
     }
-    //連接好新的node
+  
     q->rear->next = temp;
-    temp->prev = q->rear;  //++
     q->rear = temp;
-    //連接好首尾
-    q->rear->next = q->front;  //circular
-    q->front->prev = q->rear;  //circular
-    
     q->size += 1;
     return q;
+}
+
+int dequeue(struct Queue* q)
+{
+    if (q->front == NULL)
+        return 0;
+  
+    struct QNode* temp = q->front;
+    int vertex_index = temp->key;
+    q->front = q->front->next;
+
+    if (q->front == NULL)
+        q->rear = NULL;
+    
+    q->size -= 1;
+    free(temp);
+    return vertex_index;
 }
 
 
@@ -74,56 +84,54 @@ bool isInQueue(bool* InQueue, int target)
     return(InQueue[target]);
 }
 
-bool find_orNot(stack* Stack, int target)//是否能找到相對應的vertex
+bool find_orNot(vertex* Vertex, int target)//是否能找到相對應的vertex
 {
-    stack tmp = Stack[target];
+    vertex tmp = Vertex[target];
     int index = tmp.array[tmp.top];  //目前vertex上的第一個值
 
-    if (Stack[index].array[Stack[index].top] == target)  //別人的值等於我的index
+    if (Vertex[index].array[Vertex[index].top] == target)  //別人的值等於我的index
         return true;
     return false;
 }
 
-int find_Num(stack* Stack, int target)//回傳對應vertex的index
+int find_Num(vertex* Vertex, int target)//若通過find_orNot，則用此函式回傳對應vertex的index
 {
-    return Stack[target].array[Stack[target].top];
+    return Vertex[target].array[Vertex[target].top];
 }
 
 
 int main()
 {
     int numVertices; scanf("%d", &numVertices);
-    stack* Stack = calloc(numVertices + 1, sizeof(stack));  //index為0不要用
+    vertex* Vertex = calloc(numVertices + 1, sizeof(vertex));  //index為0不要用(題目規定)
     Queue* queue = createQueue();  //只需要一個queue
     queue->size = 0;
     
     int degree = 0;
-    for (int i = 1; i < numVertices + 1; i++)  //讀取input到stack，index從1開始跑
+    for (int i = 1; i < numVertices + 1; i++)  //讀取vertex，index從1開始跑
     {
-        Stack[i].top = 0;
-        scanf("%d", &Stack[i].capacity);
-        degree += Stack[i].capacity;
-        Stack[i].array = calloc(Stack[i].capacity, sizeof(int));
-        for (int j = 0; j < Stack[i].capacity; j++)
-            scanf("%d", Stack[i].array + j);
+        Vertex[i].top = 0;
+        scanf("%d", &Vertex[i].capacity);
+        degree += Vertex[i].capacity;
+        Vertex[i].array = calloc(Vertex[i].capacity, sizeof(int));
+        for (int j = 0; j < Vertex[i].capacity; j++)
+            scanf("%d", Vertex[i].array + j);
     }
 
 
     int edgeNum = degree/2;  //所有邊的數量
-    int** outcome = calloc(edgeNum, sizeof(int*));  //存輸出內容
-    bool* InQueue = calloc(numVertices + 1, sizeof(bool));  //是否曾在queue裡面過了，enqueue時需要參考
+    int** outcome = calloc(edgeNum, sizeof(int*));  //存答案
+    bool* InQueue = calloc(numVertices + 1, sizeof(bool));  //是否曾在queue裡面，enqueue時需要參考
     bool possible = true;  //判斷最後是yes or no
     
-    for(int i = 1; i < numVertices + 1; i++)//traverse一遍所有vertex，找到第一層所有可用的vertex
+    for(int i = 1; i < numVertices + 1; i++)//traverse一遍所有vertex，enqueue第一層所有可用的vertex
     {
-        if(Stack[i].capacity > 0 && find_orNot(Stack, i))
+        if(Vertex[i].capacity > 0 && find_orNot(Vertex, i) && (!isInQueue(InQueue, i)))
         {
-            int tmp = find_Num(Stack, i);//對應的vertex
-            if(!isInQueue(InQueue, i))  //沒加過才加
-            {
-                queue = enqueue(queue, i);
-                InQueue[i] = true;
-            }
+            queue = enqueue(queue, i);
+            InQueue[i] = true;
+
+            int tmp = find_Num(Vertex, i);//對應的vertex
             if(!isInQueue(InQueue, tmp))  //沒加過才加
             {
                 queue = enqueue(queue, tmp);   
@@ -132,99 +140,39 @@ int main()
         }
     }
 
-
     int count = 0;//目前累積的成功存取邊數
-    int step = 0;//拿來偵測所有可用vertex都配對失敗的狀況
-    QNode* start = queue->front; //目前成功的第一個queue(預設為front)，之後會持續更新
-    int stack_index; //同等於start->key
+    int vertex_idx;//接住dequeue的vertex值
+    int num1, num2;
     
     while(queue->size > 0)
     {
-        // printf("queue size : %d\n", queue->size);
-        step = 0;
-        stack_index = start->key;
-        if (isStackFull(Stack[stack_index]))  //check:目前的vertex是否跑完了，是的話換下一個
+        vertex_idx = dequeue(queue);
+        InQueue[vertex_idx] = false;//目前vertex_idx不在queue中
+
+        while(!(isVertexFull(Vertex[vertex_idx])) && find_orNot(Vertex, vertex_idx))//isVertexFull防止跑出去
         {
-            // printf("pass:%d\n", stack_index);
-            QNode* tmp = start;
+            int tmp = find_Num(Vertex, vertex_idx);  //對應的vertex index
+            if(!isInQueue(InQueue, tmp))  //此vertex還沒加進過queue
+            {
+                queue = enqueue(queue, tmp);   
+                InQueue[tmp] = true;
+            }
 
-            //把用完的vertex前後接起來
-            start->prev->next = start->next;
-            start->next->prev = start->prev;
-            queue->size -= 1;
-            InQueue[stack_index] = 0;
-            
-            if (start == queue->front)
-                queue->front = start->next;  //更新front成下一個
-            
-            if (start == queue->rear)
-                queue->rear = start->prev;  //更新rear成前一個
+            outcome[count] = calloc(2, sizeof(int));  //有了，記下來
+            num1 = Vertex[vertex_idx].array[Vertex[vertex_idx].top];
+            num2 = Vertex[tmp].array[Vertex[tmp].top];
 
-            if (start == queue->rear->next)
-                start = queue->front;  //回到front
-            else
-                start = start->next;
-            free(tmp);
+            outcome[count][0] = num1;
+            outcome[count][1] = num2;
+
+            Vertex[vertex_idx].top += 1;//自己往後看下一格
+            Vertex[tmp].top += 1;//別人也往後看下一格
+            count += 1;
             continue;
-        }
-
-        else //經過檢查後，第一順位的vertex還沒走完
-        {
-            while(step < queue->size)  //迴圈目的：持續找目前所有有效的vertex是否可以找到，若都不行代表失敗
-            {
-                stack_index = start->key;  //vertex會持續更新
-                // printf("stack_index :%d\n", stack_index);
-                if(!(isStackFull(Stack[stack_index])) && find_orNot(Stack, stack_index))  //目前vertex可以用而且有找到
-                {
-                    // printf("delete!\n");
-                    int tmp = find_Num(Stack, stack_index);  //對應的vertex index
-                    if(!isInQueue(InQueue, tmp))  //此vertex還沒加進過queue
-                    {
-                        queue = enqueue(queue, tmp);   
-                        InQueue[tmp] = true;
-                    }
-
-                    outcome[count] = calloc(2, sizeof(int));  //有了，記下來
-                    int num1 = Stack[stack_index].array[Stack[stack_index].top];
-                    int num2 = Stack[tmp].array[Stack[tmp].top];
-                    if (num1 < num2)
-                    {
-                        outcome[count][0] = num1;
-                        outcome[count][1] = num2;
-                    }
-                    else
-                    {
-                        outcome[count][0] = num2;
-                        outcome[count][1] = num1;
-                    }
-                    Stack[stack_index].top += 1;
-                    Stack[tmp].top += 1;
-                    count += 1;
-                    break;
-                }
-                else
-                {
-                    if (InQueue[stack_index])//可以用的vertex
-                        step += 1;
-
-                    if (start == queue->rear)  //跳到front
-                        start = queue->front;
-                    else
-                        start = start->next;
-                    
-                    continue;
-                }
-            }
-
-            if (step == queue->size)  //若上面while迴圈跑完沒成功，即代表目前所有可用vertex都無法配對
-            {
-                possible = false;  //狀況一：有stack但配對不到
-                break;
-            }
         }
     }
 
-    if (queue->size == 0 && count < edgeNum)  //狀況二：沒vertex了
+    if (count < edgeNum)//成功的邊數應該要和所有邊數相等，否則失敗
         possible = false;
     
     if (possible == false)
@@ -245,7 +193,7 @@ int main()
         }
     }
     
-    free(Stack);
+    free(Vertex);
     free(outcome);
     free(InQueue);
     
